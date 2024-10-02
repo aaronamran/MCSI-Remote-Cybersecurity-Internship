7a9b4eea12aa36afbdbb57395d1e59f0237128cd
Aaron Amran <114797937+aaronamran@users.noreply.github.com>
2024-10-02 13:56:58 +0800

    Create clickjacktest.php

---
+++ Secure Software Development/scripts/prevent-clickjacking/clickjacktest.php
@@ -0,0 +1,12 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Clickjack Test</title>
+ </head>
+ <body>
+     <h1>Clickjack Test</h1>
+     <iframe src="http://localhost/preventclickjackingapp/index.php" width="600" height="400"></iframe>
+ </body>
+ </html>
