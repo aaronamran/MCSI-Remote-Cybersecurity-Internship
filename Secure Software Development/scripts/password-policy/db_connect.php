c7806002440ccaf6c9096df0e3deb5e667b09a55
Aaron Amran <114797937+aaronamran@users.noreply.github.com>
2024-10-02 13:25:54 +0800

    Create db_connect.php

---
+++ Secure Software Development/scripts/password-policy/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+// db_connect.php
+$host = 'localhost';
+$db = 'your_database_name';
+$user = 'your_username';
+$pass = 'your_password';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
