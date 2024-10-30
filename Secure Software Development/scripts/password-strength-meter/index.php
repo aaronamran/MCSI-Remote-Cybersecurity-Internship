e8d092b1eb1e363a5502d7cb15b54b94aaefbb4b
Aaron Amran <114797937+aaronamran@users.noreply.github.com>
2024-10-30 10:17:44 +0800

    Create index.php

---
+++ Secure Software Development/scripts/password-strength-meter/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Secure Web Application</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Welcome to the Secure Web Application</h1>
+    <nav>
+        <a href="register.php">Register</a> | 
+        <a href="login.php">Login</a> | 
+        <a href="update_password.php">Update Password</a>
+    </nav>
+</body>
+</html>
