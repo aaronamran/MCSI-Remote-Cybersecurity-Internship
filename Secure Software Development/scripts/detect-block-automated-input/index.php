9337b66040bc316da17183c5505ad4e6e6a0a9ae
Aaron Amran <114797937+aaronamran@users.noreply.github.com>
2024-11-02 11:29:38 +0800

    Create index.php

---
+++ Secure Software Development/scripts/detect-block-automated-input/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Secure Web Application</title>
+</head>
+<body>
+    <h2>Welcome to the Secure Web Application</h2>
+    <p>Select a form to fill out:</p>
+    <ul>
+        <li><a href="form1.php">Form 1</a></li>
+        <li><a href="form2.php">Form 2</a></li>
+        <li><a href="form3.php">Form 3</a></li>
+        <li><a href="form4.php">Form 4</a></li>
+        <li><a href="form5.php">Form 5</a></li>
+    </ul>
+</body>
+</html>
