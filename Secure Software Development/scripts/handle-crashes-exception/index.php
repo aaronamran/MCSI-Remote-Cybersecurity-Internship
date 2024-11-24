b5298b817af197c3b0d559092b840140df4be3d6
Aaron Amran <114797937+aaronamran@users.noreply.github.com>
2024-11-24 12:30:16 +0800

    Update index.php

--- Secure Software Development/scripts/handle-crashes-exception/index.php
+++ Secure Software Development/scripts/handle-crashes-exception/index.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "vulnerable_app";
+$dbname = "handleerrorapp";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
