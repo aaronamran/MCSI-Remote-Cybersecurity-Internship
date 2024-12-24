d024812708f1eb75d4bfb4c461c05646120e4043
Aaron Amran <114797937+aaronamran@users.noreply.github.com>
2024-12-25 02:03:02 +0800

    Delete Secure Software Development/scripts/detect-block-automated-input directory

--- Secure Software Development/scripts/detect-block-automated-input/captcha.js
+++
@@ -1,8 +0,0 @@
-function validateCaptcha() {
-    const captchaInput = document.querySelector('input[name="captcha_input"]').value;
-    if (captchaInput.trim() === '') {
-        alert('Please enter the CAPTCHA.');
-        return false;
-    }
-    return true;
-}
